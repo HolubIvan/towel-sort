99b6883f7900c590680b4407f386289a23868ba0
Ivan Holub <ivanholub.js@gmail.com>
2020-03-18 12:00:59 +0200

    feat: resolve towel-sort test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,29 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined || matrix.length ===0){
+    return [];
+  }
+  if(matrix.length === 1){
+      return matrix;
+  }
+
+  let array = [];
+  let reverse = false;
+
+  matrix.forEach(function(el){
+    if(!reverse){
+        el.forEach(function(e){
+            array.push(e)
+        })
+        reverse = true;
+    } else{
+        el.reverse().forEach(function(e){
+            array.push(e);
+        })
+        reverse = false;
+    }
+  })
+  
+  return array;
 }
